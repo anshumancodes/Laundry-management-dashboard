c1a24e48579286655a65777b2e38dcebe925d891
anshumancodes <anshumanprof01@gmail.com>
2024-02-09 23:57:44 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Laundry-management-dashboard
 simple Laundry-management-dashboard for University dormitory 
+
+
+## Install dependencies
+
+```bash
+
+git clone <git-repo-url>
+
+#for client dir
+cd client
+npm i
+
+#for server
+cd server
+
+pip install -r requirements.txt
+
+
+
+
+```
---
+++ server/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return "adding data....."
+    else:
+        return f"Welcome to apna laundry"
+@app.route('/support')
+def support():
+    return 'Welcome to the support page!'
+
+
+
+@app.route('/billing')
+def billing():
+    return 'Welcome to the billing page!'
+
+@app.route('/inventory')
+def inventory():
+    return 'Welcome to the inventory page!'
+
+@app.route('/payments')
+def payments():
+    return 'Welcome to the payments page!'
+
+if __name__ == '__main__':
+    print("Server started")
+    app.run( port=8000, debug=True)
