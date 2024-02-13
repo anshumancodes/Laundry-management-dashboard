86fad6d9a4eb67fe453fbc852ff0da55ee323d4d
anshumancodes <anshumanprof01@gmail.com>
2024-02-13 12:53:15 +0530

    modified routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,10 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy } from "react";
 
+
 const Home = lazy(() => import("./components/Landing page/Home"));
+const Inventory = lazy(() => import("./components/Inventory/Inventory"));
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/DashBoard" element={<Inventory />} />
           </Routes>
         </Suspense>
       </Router>
