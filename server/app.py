81807087aacc92d39f2f084a0241d65971a4f4a6
anshumancodes <anshumanprof01@gmail.com>
2024-02-15 01:22:27 +0530

    enabled cors , modified json data that to be rendered

--- server/app.py
+++ server/app.py
@@ -1,27 +1,45 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 
-
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
     return f"welcome to suddhi home page"
 
-
-
 @app.route('/inventory', methods=['GET'])
 def inventory():
-     with open('laundry_data.json') as f:
+    with open('laundry_data.json') as f:
         laundry_data = json.load(f)
-        if(request.method=='GET'):
-             return jsonify(laundry_data)
-   
-      
-
- 
+        isActive = 0  # Initialize the counter for active items
+        isreceived = 0  
+        yetToReceive=isActive-isreceived
+        
+        for item in laundry_data:
+            if item["Active"]:
+                isActive += 1
+                # isreceived-=isreceived
+            if item["received"]:
+                isreceived+=1  
        
-    
+          
+
+            sumData={
+                 "active_count": isActive,
+                 "received_Total":isreceived,
+                 "yet_toRecieve":isActive-isreceived
+
+            }
+                
+        return jsonify(sumData)
+
+
+
+
+
+
 
 @app.route('/support')
 def support():
@@ -30,8 +48,6 @@ def support():
 @app.route('/billing')
 def billing():
     return 'Welcome to the billing page!'
-
-
 
 @app.route('/payments')
 def payments():
