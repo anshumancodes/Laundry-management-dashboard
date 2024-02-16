1f5d5d94b13fbc084e4bbb4d55a5d79163c6d4c0
anshumancodes <anshumanprof01@gmail.com>
2024-02-16 17:57:19 +0530

    imported and added new Component route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy } from "react";
 
 
+
 const Home = lazy(() => import("./components/Landing page/Home"));
 const Inventory = lazy(() => import("./components/Inventory/Inventory"));
+const Billing =lazy(()=>import("./components/billing/Billing"));
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/DashBoard" element={<Inventory />} />
+            <Route path="/DashBoard/billing" element={<Billing/>} />
           </Routes>
         </Suspense>
       </Router>
