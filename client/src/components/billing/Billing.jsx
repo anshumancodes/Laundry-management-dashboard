c0d05be813ef13759782da4c3dc870a380cad91b
anshumancodes <anshumanprof01@gmail.com>
2024-02-16 17:56:39 +0530

    created Billing.jsx

---
+++ client/src/components/billing/Billing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from '../global/SideBar'
+const Billing = () => {
+  return (
+    <div>
+        <div className='flex gap-[4vw]'>
+            <SideBar/>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Billing
