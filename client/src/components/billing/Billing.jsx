8e84db9f7115b2a919e0252684cb5d5600472654
anshumancodes <anshumanprof01@gmail.com>
2024-02-17 17:46:03 +0530

    added order creating form

--- client/src/components/billing/Billing.jsx
+++ client/src/components/billing/Billing.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import SideBar from '../global/SideBar'
-const Billing = () => {
+import React from "react";
+import SideBar from "../global/SideBar";
+import Selector from "../global/Selector";
+import BillingForm from "./BillingForm";
+
+const Billing = ({ActiveOrders}) => {
   return (
     <div>
-        <div className='flex gap-[4vw]'>
-            <SideBar/>
+      <div className="flex gap-[4vw]">
+        <SideBar />
+
+        <div className="mt-[4vh] flex flex-col gap-[4vh]">
+          <Selector activeorders={ActiveOrders} />
+
+          <div className="bg-[#0C121C] flex flex-col gap-2 items-center justify-center" >
+            <div className="bg-[#7148FC] w-[400px] px-4 py-2 font-bold text-white mt-5 text-center">Create New Order</div>
+            <form action="POST">
+              <BillingForm/>
+
+            </form>
+          </div>
 
         </div>
-      
-    </div>
-  )
-}
 
-export default Billing
+      </div>
+    </div>
+  );
+};
+
+export default Billing;
