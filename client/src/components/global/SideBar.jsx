9d8aa7b669a44f49eaf8129d8db275913f91c78a
anshumancodes <anshumanprof01@gmail.com>
2024-02-23 23:52:02 +0530

    modified links in sidebar.jsx

--- client/src/components/global/SideBar.jsx
+++ client/src/components/global/SideBar.jsx
@@ -4,7 +4,7 @@ import { IoIosLogOut } from "react-icons/io";
 const SideBar = () => {
   return (
     <div>
-      <div className='flex-col flex  bg-[#1E293B] h-[100vh]'>
+      <div className='flex-col flex  bg-[#1E293B] min-h-screen'>
         <div className=' px-12 py-7 bg-[#0C121C]'>
             <h1 className='text-white lg:text-[3rem] font-extrabold'>Shuddhi शुद्धि</h1>
         </div>
@@ -12,10 +12,10 @@ const SideBar = () => {
 
         </div>
         <div className='flex-col flex gap-[7vh] w-full  text-white mt-5 font-bold text-2xl text-left px-12'>
-            <a href="">Inventory</a>
-            <a href="">billing</a>
-            <a href="">Payments</a>
-            <a href="">Support</a>
+            <a href="/dashboard/inventory">Inventory</a>
+            <a href="/dashboard/billing">billing</a>
+            <a href="/dashboard/payment">Payments</a>
+            <a href="/Support">Support</a>
             <a href="" className=' w-fit  font-light py-2 flex items-center gap-2'><button>log out </button> <IoIosLogOut /></a>
 
         </div>
